693c7325128c6ccd27e020521ba1e7e78bd5f982
Jason Brown <jasonbrown@fastmail.fm>
2020-03-03 08:31:44 -0500

    implemented the spy_game function

--- section6methods/44functionpractice.py
+++ section6methods/44functionpractice.py
@@ -3,5 +3,19 @@
 # spy_game([1,0,2,4,0,5,7]) --> True
 # spy_game([1,7,2,0,4,5,0]) --> False
  
-def spy_game(int_seq: list(int)) -> boolean:
-    return False
+def spy_game(find_seq, in_seq) -> bool:
+    found_counter = 0
+    in_seq_index = 0
+    while (found_counter < len(find_seq) and in_seq_index < len(in_seq)):
+        if (find_seq[found_counter] == in_seq[in_seq_index]):
+            found_counter += 1
+        in_seq_index += 1
+            
+    return found_counter == len(find_seq)
+
+
+print(spy_game([0,0,7], [1,2,4,0,0,7,5]))
+print(spy_game([0,0,7], [1,0,2,4,0,5,7]))
+print(spy_game([0,0,7], [1,7,2,0,4,5,0]))
+
+
