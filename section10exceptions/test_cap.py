3314ddf12165d597656e78d1f8ed17c606defd6e
Jason Brown <jasonbrown@fastmail.fm>
2020-03-08 10:20:42 +0100

    added comments to improve pyint score to 10/10!

--- section10exceptions/test_cap.py
+++ section10exceptions/test_cap.py
@@ -1,19 +1,29 @@
+'''
+    Simple example of using unittest for Python
+'''
 import unittest
 import cap
 
 class TestCap(unittest.TestCase):
-
+    '''
+        Unit test for the capitalization function
+    '''
     def test_one_word(self):
+        '''
+        Test a single work is capitalized
+        '''
         text = 'python'
         result = cap.cap_text(text)
         self.assertEqual(result, 'Python')
 
     def test_two_words(self):
+        '''
+        Test both words of a two word string are capitalized
+        '''
         text = 'monty python'
         result = cap.cap_text(text)
         self.assertEqual(result, 'Monty Python')
 
 if __name__ == '__main__':
     unittest.main()
-
-
+    
